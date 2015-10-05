7cf022daa48a1f1b6c68344f6943bae932347386
clonq <ovi.comes@gmail.com>
2015-10-05 09:01:29 -0400

    added configurable item template

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
-$(function(){
-	$(document).on('click', '#clonq-login .register-link', function() {
-		revo.load({component:"clonq_user/register"});
-	});
-})
+clonq_revo_list_ui = {
+    init: function(config){
+        //todo: fix reload
+        if(!!config.templates && !!config.templates.item) {
+            $(document.createElement('script')).attr({id:"revo-list-ui-item-template",type:"text/x-handlebars-template"}).appendTo($('head'));
+            $('#revo-list-ui-item-template').load(config.templates.item);
+        }
+        document.addEventListener("revo:ready", function (e) {
+            if(!!config.dataLoadEvent) {
+                var itemTemplateSrc = $("#revo-list-ui-item-template").text();
+                var itemTemplate = Handlebars.compile(itemTemplateSrc);
+                document.addEventListener(config.dataLoadEvent, function (e) {
+                    var items = e.detail;
+                    items.forEach(function(itemData){
+                        var itemHtml = itemTemplate(itemData);        
+                        $('#revo-list-ui .grid').append(itemHtml);
+                    });
+                });
+            } else {
+                console.log('clonq/revo-list-ui: no dataLoadEvent key')
+            }
+        });        
+    }
+}
